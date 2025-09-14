58c63ec6524e71b78fabbd5148f1cd8266679fe7
NIHAL <nilufdev@gmail.com>
2025-09-14 17:43:23 +0530

    Create foods.dart

---
+++ foods.dart
@@ -0,0 +1,30 @@
+final List<Map<String, Object>> foods = [
+  {
+    'bgColor': '',
+    'title': '',
+    'subtitle': '',
+    'price', 80,
+    'imgPath': '',
+  },
+  {
+    'bgColor': '',
+    'title': '',
+    'subtitle': '',
+    'price', 120,
+    'imgPath': '',
+  },
+  {
+    'bgColor': '',
+    'title': '',
+    'subtitle': '',
+    'price', 50,
+    'imgPath': '',
+  },
+  {
+    'bgColor': '',
+    'title': '',
+    'subtitle': '',
+    'price', 100,
+    'imgPath': '',
+  },
+];
