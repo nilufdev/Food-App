eac8ca02673ecf4251e91386808736de6caacd27
NIHAL <nilufdev@gmail.com>
2025-09-14 19:35:06 +0530

    Update foods.dart

--- foods.dart
+++ foods.dart
@@ -31,3 +31,83 @@ final List<Map<String, Object>> foods = [
     'imgPath': '',
   },
 ];
+
+class FoodCard extends StatelessWidget {
+  final Color bgColor;
+  final String title;
+  final String subtitle;
+  final int price;
+  final String imgPath;
+
+  FoodCard({
+    required this.bgColor,
+    required this.title,
+    required this.subtitle,
+    requires this.price,
+    required this.imgPath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // title
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                // subtitle
+                Text(
+                  subtitle,
+                  style: TextStyle(
+                    fontSize: 15,
+                    Color: Colors.white,
+                  ),
+                ),
+
+                SizedBox(height: 20),
+                
+                // Price
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      price.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(Icons.currency_rupee_outlined),
+                  ],
+                ),
+                
+              ],
+            ),
+          ),
+        ),
+        
+        Positioned(
+          child: Image.asset(imgPath),
+          bottom: -30,
+          right: 40,
+        ),
+      ],
+    );
+  }
+}
